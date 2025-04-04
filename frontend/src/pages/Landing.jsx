34c4a4c3e48a9aea62d8bb47bc5a0dc329ff28ae
Harsh Depura <harsh.devel0per.11@gmail.com>
2025-04-05 00:00:19 +0530

    landing page done

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,11 +1,16 @@
 import "./landing.css"
 import styled from "styled-components";
-import image from "./frontend/public/Green-bou.png"
+import image from "../assets/Green-bou.png"
+import circles from "../assets/3-circles.jpg"
+import triangle from "../assets/Green_equilateral_triangle_point_up.svg.png"
 
 const DIV=styled.div``;
 const P=styled.p``;
 const BUTTON=styled.button``;
 const IMG=styled.img``;
+const BR=styled.br``;
+const SPAN=styled.span``;
+const H2=styled.h2``;
 function Landing()
 {
     return(
@@ -22,10 +27,31 @@ function Landing()
             <DIV className={"cont"}>
                <DIV className={"half-container"}>
                    <IMG src={image} alt={"green-image"}></IMG>
-
                </DIV>
                <DIV className={"half-container"}>
-
+                   <P className={"Headline"}>
+                       <SPAN className={"shadow-text"}>Control</SPAN> Your
+                       <BR/><SPAN className={"shadow-text"}>Financial</SPAN> Future Easily
+                       <BR/> With Expense Manager App
+                   </P>
+                   <P className={"app-description"}>
+                       Your all-in-one solution to track spending, set budgets, and build better financial habits.
+                       Open your account in just a few clicks.
+                   </P>
+                   <DIV className={"Open-acc"}>
+                       <BUTTON>Open Account</BUTTON>
+                       <BUTTON className={"Generate-card"}> Generate Your Card</BUTTON>
+                   </DIV>
+                   <DIV className={"three-circles"}>
+                       <IMG src={circles} alt={"3-faces"}></IMG>
+                       <IMG  className={"triangle"} src={triangle} alt={"green-triangle"}></IMG>
+                       <P className={"app-status"}><H2>20 Thousand+</H2>
+                           <BR/>
+                           <SPAN className={"shadow-text"}>Join over 20 thousand + satisfied users
+                                                      <BR/>who trust this app for managing
+                                                      <BR/>their finance.
+                           </SPAN></P>
+                   </DIV>
                </DIV>
             </DIV>
 
